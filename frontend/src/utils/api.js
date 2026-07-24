230fa4af392f17fbabe92b12869fbf5fbb9bff2b
harshparmar009 <hardinmartin09@gmail.com>
2026-07-24 09:52:55 +0530

    Fix frontend API connection again

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const API_BASE_URL =
   import.meta.env.VITE_API_URL || "http://127.0.0.1:5000";
 
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+console.log("FINAL API BASE URL =", `${API_BASE_URL}/api`);v
+
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api`,
   headers: {
